d736b46b9dc93b238c3af8b62328e70ada52be8c
zhangyuanxin <stark.zhang@jinfuzi.com>
2016-05-05 19:31:53 +0800

    'redis_stringserializer'

--- src/main/java/com/zyx/redis/listener/RedisMessageListener.java
+++ src/main/java/com/zyx/redis/listener/RedisMessageListener.java
@@ -3,26 +3,23 @@ package com.zyx.redis.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-
-import java.io.UnsupportedEncodingException;
+import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * Created by zhangyuanxin on 2016/3/30.
  */
 public class RedisMessageListener implements MessageListener {
     private Logger log = LoggerFactory.getLogger(RedisMessageListener.class);
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        String channel = "";
-        try {
-            channel = new String(pattern, "utf-8");
-        } catch (UnsupportedEncodingException e) {
-            log.error("wrong occurred while decode channel " + e);
-        }
-        log.info("new activation msg:" + message);
+        String msg = (String)redisTemplate.getValueSerializer().deserialize(message.getBody());
+        String channel = (String)redisTemplate.getStringSerializer().deserialize(pattern);
+        log.info("new msg:" + msg + " channel:" + channel);
     }
 
 
